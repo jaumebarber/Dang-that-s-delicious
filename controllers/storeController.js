cb6d9919465eb249398aa44eeab0e6159fc0e168
Jaume Barber <jaumebarber@gmail.com>
2019-03-18 02:12:17 +0100

    site section title fixed

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Store = mongoose.model('Store')
 
 exports.homePage = (req, res) => {
-  res.render('index')
+  res.render('index', { title: 'Home' })
 }
 
 exports.addStore = (req, res) => {
