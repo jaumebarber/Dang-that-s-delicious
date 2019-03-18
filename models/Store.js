821c9352e78aa14c3752f61140d42431fa047cfc
Jaume Barber <jaumebarber@gmail.com>
2019-03-18 18:42:05 +0100

    added long, lat and address fields to view and model

--- models/Store.js
+++ models/Store.js
@@ -4,10 +4,33 @@ const Schema = mongoose.Schema
 const slug = require('slugs')
 
 const storeSchema = new Schema({
-  name: { type: String, trim: true, required: 'Please enter a store name!' },
+  name: { 
+    type: String, 
+    trim: true, 
+    required: 'Please enter a store name!' 
+  },
   slug: String,
-  description: { type: String, trim: true },
-  tags: [String]
+  description: { 
+    type: String, 
+    trim: true 
+  },
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: String,
+    default: 'Point'
+  },
+  coordinates: [{
+    type: Number,
+    required: 'You must supply coordinates!'
+  }],
+  address: {
+    type: String,
+    required: 'You must supply an address!'
+  }
 })
 
 storeSchema.pre('save', function(next) {
