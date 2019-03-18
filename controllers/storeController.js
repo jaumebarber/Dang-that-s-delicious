307954649924428bac61910c98ef4e2cf67a2d12
Jaume Barber <jaumebarber@gmail.com>
2019-03-18 17:41:09 +0100

    updateStore functionality added

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -11,11 +11,25 @@ exports.addStore = (req, res) => {
 
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save()
-  res.flash('succes', `Successfully Created ${store.name}. Care to leave a review?`)
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`)
   res.redirect(`/store/${store.slug}`)
 }
 
 exports.getStores = async (req, res) => {
   const stores = await Store.find()
   res.render('stores', { title: 'Stores', stores })
+}
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id })
+  res.render('editStore', { title: `Edit ${store.name}`, store })
+}
+
+exports.updateStore = async (req, res) => {
+  const store = await Store
+    .findOneAndUpdate({ _id: req.params.id }, req.body, { 
+      new: true, runValidators: true })
+    .exec()
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>! <a href="/stores/${store.slug}">View Store</a>`)
+  res.redirect(`/stores/${store._id}/edit`)
 }
