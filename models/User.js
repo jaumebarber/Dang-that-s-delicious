e7aeebe21bc668bf7df46bb3f4cbab42e4ddbd5d
Jaume Barber <jaumebarber@gmail.com>
2019-03-27 11:58:45 +0100

    hearts functionality implemented

--- models/User.js
+++ models/User.js
@@ -26,7 +26,8 @@ const userSchema = new Schema({
     trim: true
   },
   resetPasswordToken: String,
-  resetPasswordExpires: Date
+  resetPasswordExpires: Date,
+  hearts: [{ type: mongoose.Schema.ObjectId, ref: 'Store' }]
 })
 
 userSchema.virtual('gravatar').get(function() {
--- routes/index.js
+++ routes/index.js
@@ -47,5 +47,6 @@ router.post('/account/reset/:token',
 router.get('/api/search', catchErrors(storeController.searchStores))
 router.get('/api/stores/near', catchErrors(storeController.mapStores))
 router.get('/map', storeController.mapPage)
+router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore))
 
 module.exports = router
